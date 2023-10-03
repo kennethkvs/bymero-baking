473b3485b6a1ac19932f1b42e46480b0f83b1657
kennethkvs <kennethkvs060103@gmail.com>
2023-10-03 12:02:58 -0400

    Fix Menu

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -5,18 +5,21 @@ export default function Menu() {
     {
       id: 0,
       name: "Nastar",
+      spelling: "/nasˈtɑɹ/",
       desc: "Lorem Ipsum dll sajdfjasdfkjajkf ajsdfj sdjfajsdf sjdfadjf",
       img: "/images/cookies.jpg",
     },
     {
       id: 1,
-      name: "Kaastengel",
+      name: "Kaasstengel",
+      spelling: "/kaasstɛŋɡɛl/",
       desc: "Lorem Ipsum dll sajdfjasdfkjajkf ajsdfj sdjfajsdf sjdfadjf",
       img: "/images/cookies.jpg",
     },
     {
       id: 2,
       name: "Cookies",
+      spelling: "/ˈkʊkiz/",
       desc: "Lorem Ipsum dll sajdfjasdfkjajkf ajsdfj sdjfajsdf sjdfadjf",
       img: "/images/cookies.jpg",
     },
@@ -27,8 +30,9 @@ export default function Menu() {
       key={item.id}
       className="m-4 flex grow flex-col flex-wrap items-center justify-center rounded-md bg-slate-200"
     >
-      <img src={item.img} className="" />
+      <img src={item.img} className="rounded-md" />
       <div className="mt-4 text-2xl font-bold">{item.name}</div>
+      <div className="mt-0 text-center">{item.spelling}</div>
       <div className="m-4 text-center">{item.desc}</div>
     </div>
   ));
